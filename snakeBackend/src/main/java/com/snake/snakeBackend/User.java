b6b5e92b88f413c266ca00c7215f6c3ff047de3a
mlinhou <mlinhou@gmail.com>
2024-08-08 12:00:24 -0500

    post request

---
+++ snakeBackend/src/main/java/com/snake/snakeBackend/User.java
@@ -0,0 +1,15 @@
+package com.snake.snakeBackend;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document(collection = "Players")
+public class User {
+    @Id
+    private String id;
+    private String name;
+    private int age;
+
+}
---
+++ snakeBackend/src/main/java/com/snake/snakeBackend/UserService.java
@@ -0,0 +1,32 @@
+package com.snake.snakeBackend;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(String id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User updateUser(String id, User user) {
+        user.setId(id);
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(String id) {
+        userRepository.deleteById(id);
+    }
+}
