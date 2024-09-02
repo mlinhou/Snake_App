730487e0dad7094c1fc8d4caf12a768d6d91c9c1
mlinhou <mlinhou@gmail.com>
2024-09-02 14:41:11 -0500

    remove unused code

--- snakeBackend/src/main/java/com/snake/snakeBackend/UserController.java
+++ snakeBackend/src/main/java/com/snake/snakeBackend/UserController.java
@@ -1,7 +1,6 @@
 package com.snake.snakeBackend;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,31 +11,6 @@ public class UserController {
 
     @Autowired
     private UserService userService;
-
-    private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
-
-    public UserController(UserRepository userRepository, PasswordEncoder passwordEncoder) {
-        this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
-    }
-
-    @PostMapping("/register")
-    public String registerUser(@RequestBody User user) {
-        if (userRepository.findByUsername(user.getUsername()) != null) {
-            return "User already exists!";
-        }
-        user.setUsername(user.getUsername());
-        user.setPassword(passwordEncoder.encode(user.getPassword()));
-        userRepository.save(user);
-        return "User registered successfully!";
-    }
-
-    @PostMapping("/login")
-    public String loginUser() {
-        // Authentication is handled automatically by Spring Security
-        return "Login successful!";
-    }
 
     @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping
